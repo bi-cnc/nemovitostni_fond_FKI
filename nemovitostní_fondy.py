1cb830ffb5ac258494d105c1f9e4e28c9f6a3309
bi-cnc <137881662+bi-cnc@users.noreply.github.com>
2023-12-19 10:48:30 +0100

    Update nemovitostní_fondy.py

--- nemovitostní_fondy.py
+++ nemovitostní_fondy.py
@@ -17,7 +17,7 @@ from streamlit.components.v1 import html
 # Custom HTML and CSS
 custom_html = """
 <div style="display: flex; align-items: center; justify-content: space-between;">
-    <h1>Fondy kvalifikovaných investorů</h1>
+    <h1 class="st-ae" style="font-size: 1.5em; font-weight: bold;">Fondy kvalifikovaných investorů</h1>
     <a href="https://fullscreen-fki.streamlit.app/" target="_blank" title="Otevři fullscreen aplikace">
         <img src="https://cdn1.iconfinder.com/data/icons/material-core/14/fullscreen-512.png" alt="Fullscreen" style="height: 30px; width: 30px;">
     </a>
@@ -26,6 +26,7 @@ custom_html = """
 
 # Inject the custom HTML into Streamlit
 html(custom_html)
+
 
 # Load the data
 @st.cache_data
