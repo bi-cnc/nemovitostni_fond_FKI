9065319fa153641cf2b8ccf2d5de436aeece9cd5
bi-cnc <137881662+bi-cnc@users.noreply.github.com>
2023-12-19 13:50:48 +0100

    Update nemovitostní_fondy.py

--- nemovitostní_fondy.py
+++ nemovitostní_fondy.py
@@ -18,7 +18,7 @@ from streamlit.components.v1 import html
 # Custom HTML and CSS
 custom_html = """
 <div style="margin-bottom: 0px; display: flex; align-items: center; justify-content: space-between;">
-    <h1 style="font-family: 'IBM Plex Sans', sans-serif; font-size: 23px; font-weight: 600; color: #262730; margin-right: 30px; margin: 0px;">Fondy kvalifikovaných investorů</h1>
+    <h1 style="font-family: 'IBM Plex Sans', sans-serif; font-size: 22px; font-weight: 600; color: #262730; margin-right: 30px; margin: 0px;">Fondy kvalifikovaných investorů</h1>
     <a href="https://fullscreen-fki.streamlit.app/" target="_blank" title="Otevři fullscreen aplikace">
         <img src="https://cdn1.iconfinder.com/data/icons/material-core/14/fullscreen-512.png" alt="Fullscreen" style="height: 30px; width: 30px;">
     </a>
@@ -448,7 +448,7 @@ st.markdown("<br>", unsafe_allow_html=True)
 st.markdown("<br>", unsafe_allow_html=True)
 retail_html = """
 <div style="margin-bottom: 0px; display: flex; align-items: center; justify-content: space-between;">
-    <h1 style="font-family: 'IBM Plex Sans', sans-serif; font-size: 24px; font-weight: 600; color: #262730; margin-right: 30px; margin: 0px;">Retailové fondy</h1>
+    <h1 style="font-family: 'IBM Plex Sans', sans-serif; font-size: 22px; font-weight: 600; color: #262730; margin-right: 30px; margin: 0px;">Retailové fondy</h1>
 </div>
 """
 
